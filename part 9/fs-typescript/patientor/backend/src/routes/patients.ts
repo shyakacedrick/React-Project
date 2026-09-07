99de61d0eb12304b1eb13591d34369b47ec54caa
shyakacedrick <student@sirafrica.org>
2026-09-07 11:10:06 +0300

    Exercise 9.12: Patientor backend step 4 - data validation and type guards

--- part 9/fs-typescript/patientor/backend/src/routes/patients.ts
+++ part 9/fs-typescript/patientor/backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
 import express, { type Response } from 'express';
 import patientService from '../services/patientService.ts';
-import type { NonSensitivePatientEntry, Patient, NewPatientEntry } from '../types.ts';
+import type { NonSensitivePatientEntry, Patient } from '../types.ts';
+import toNewPatientEntry from '../utils.ts';
 
 const router = express.Router();
 
@@ -8,10 +9,18 @@ router.get('/', (_req, res: Response<NonSensitivePatientEntry[]>) => {
   res.send(patientService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res: Response<Patient>) => {
-  const newPatientData = req.body as NewPatientEntry;
-  const addedPatient = patientService.addPatient(newPatientData);
-  res.json(addedPatient);
+router.post('/', (req, res: Response<Patient | { error: string }>) => {
+  try {
+    const newPatientEntry = toNewPatientEntry(req.body);
+    const addedPatient = patientService.addPatient(newPatientEntry);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send({ error: errorMessage });
+  }
 });
 
 export default router;
